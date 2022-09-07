ae7af78967fddc551912d41b69ff9a7e7d512c52
Vanessa Power <vanessa.l.power@gmail.com>
2022-09-06 17:12:29 -0700

    adding the constants module

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
